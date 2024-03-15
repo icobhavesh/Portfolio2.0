9a5ec96c69d519c3380146f4b26876e805cfb663
icobhavesh <bhaveshsaxena058@gmail.com>
2024-03-15 15:00:19 +0530

    done

--- src/Components/ProjectData.jsx
+++ src/Components/ProjectData.jsx
@@ -1,11 +1,10 @@
 const ProjectData = [
     {
-        images: "../assets/CryptoCurrency.png",
-
-        Name: "Shopping-Website",
+        images: "../assets/Ecommerceweb.png",
+        Name: " Shopping-Website",
         Description:
-            "This is a shopping app where I utilized React-Redux-Toolkit and React Router DOM. I implemented features such as add to cart and checkout functionality.",
-        link: 'https://shopping-3ub2jr5bm-bhavesh-s-projects.vercel.app/'
+            "I developed a cryptocurrency tracker app where users can monitor the crypto market and visualize market trends through graphs.",
+        link: "https://bitcoin-project-pi.vercel.app"
     },
     {
         images: "../assets/youtube clone.png",
@@ -15,12 +14,14 @@ const ProjectData = [
         link: "https://ytclone-iota.vercel.app/"
     },
     {
-        images: "../assets/Ecommerceweb.png",
+        images: "../assets/CryptoCurrency.png",
+
         Name: "CryptoCurrency-Tracker",
         Description:
-            "I developed a cryptocurrency tracker app where users can monitor the crypto market and visualize market trends through graphs.",
-        link: "https://bitcoin-project-pi.vercel.app"
+            "This is a shopping app where I utilized React-Redux-Toolkit and React Router DOM. I implemented features such as add to cart and checkout functionality.",
+        link: 'https://shopping-3ub2jr5bm-bhavesh-s-projects.vercel.app/'
     },
+
 ];
 
 export default ProjectData;
