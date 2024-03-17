8683fb8eee8b74835e24c691970a921e289ce4ef
bhavesh saxena <149712436+icobhavesh@users.noreply.github.com>
2024-03-17 21:42:41 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,56 +1,52 @@
-
-import React, { useState } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { Link } from "react-router-dom"
 import ParticlesBg from "./Particles";
+import "../index.css"
 const Header = () => {
-    const [toggle, setToggle] = useState(false)
-    //bg-blue-600
+    const [toggle, setToggle] = useState(false);
+    
+    const handleLinkClick = () => {
+        // Close the navbar when a link is clicked
+        setToggle(false);
+    };
+
     return (
-        <div className="    w-full p-4  ">
+        <div className="w-full p-4">
             <ParticlesBg />
-            <div className=" max-w-[1240px] h-200px  flex justify-between items-center m-auto">
-                <div className="space-grotesk  text-white md: text-[30px] ">
+            <div className="max-w-[1240px] h-200px flex justify-between items-center m-auto">
+                <div className="text-white md:text-[30px] space-grotesk">
                     <h1>Bhavesh</h1>
                 </div>
                 {/* ToggleSection */}
                 {toggle ? (
                     <AiOutlineClose
                         onClick={() => setToggle(!toggle)}
-                        className="text-white text-2x md:hidden block "
+                        className="text-white text-2x md:hidden block"
                     />
                 ) : (
                     <AiOutlineMenu
                         onClick={() => setToggle(!toggle)}
-                        className="text-white text-2x md:hidden block "
+                        className="text-white text-2x md:hidden block"
                     />
                 )}
-                {/*  hidden for sm device  */}
-                <ul className=" 
-space-grotesk text-bolder hidden md:flex gap-10 text-white  hover:underline ml-0 text-xl  ">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/project">Project</Link>
-                    <Link to="/contact">Contact</Link>
+                {/* hidden for sm device */}
+                <ul className="space-grotesk text-bolder hidden md:flex gap-10 text-white hover:underline hover:text-purple-500 cursor-pointer navLink ml-0 text-xl">
+                    <Link to="/" onClick={handleLinkClick}>Home</Link>
+                    <Link to="/about" onClick={handleLinkClick}>About</Link>
+                    <Link to="/project" onClick={handleLinkClick}>Project</Link>
+                    <Link to="/contact" onClick={handleLinkClick}>Contact</Link>
                 </ul>
-                {/* //////////navbar for sm using toogle button ///////// */}
-                <ul
-                    className={`"  anton-regular  
-                duration-300 md:hidden gap-4  flex justify-between text-white bg-slate-950 h-autofixed text-xl 
-                top-[58px]  left-[-100%] w-full ${toggle ? "left-[0]" : "left-[-100%]  "
-                        }`}
-                >
-                    <Link to="/" className=" space-grotesk p-3 text-center ">Home</Link>
-                    <Link to="/about" className=" space-grotesk p-3 text-center ">About</Link>
-                    <Link to="/project" className=" space-grotesk p-3 text-center ">Project</Link>
-                    <Link to="/contact" className=" space-grotesk p-3 text-center ">Contact</Link>
-
-
+                {/* //////////navbar for sm using toggle button ///////// */}
+                <ul className={`duration-300 md:hidden gap-2 l navLink text-white flex flex-col bg-slate-950 h-auto fixed text-xl top-[58px] left-[-100%] w-full ${toggle ? "left-[0]" : "left-[-100%]"}`}>
+                    <Link to="/" className="space-grotesk p-3 text-center" onClick={handleLinkClick}>Home</Link>
+                    <Link to="/about" className="space-grotesk p-3 text-center" onClick={handleLinkClick}>About</Link>
+                    <Link to="/project" className="space-grotesk p-3 text-center" onClick={handleLinkClick}>Project</Link>
+                    <Link to="/contact" className="space-grotesk p-3 text-center" onClick={handleLinkClick}>Contact</Link>
                 </ul>
             </div>
         </div>
     );
 };
-
 
 export default Header;
