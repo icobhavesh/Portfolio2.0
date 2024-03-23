684e751f96f7a878b834098cef6a4d80ef5b862e
bhavesh saxena <149712436+icobhavesh@users.noreply.github.com>
2024-03-23 12:33:13 +0530

    Update ProjectData.jsx

--- src/Components/ProjectData.jsx
+++ src/Components/ProjectData.jsx
@@ -3,8 +3,7 @@ const ProjectData = [
         images: "../assets/Ecommerceweb.png",
         Name: " Shopping-Website",
         Description: "This is a shopping app where I utilized React-Redux-Toolkit and React Router DOM. I implemented features such as add to cart and checkout functionality.",
-
-        link: "https://bitcoin-project-pi.vercel.app"
+        link: 'https://shopping-3ub2jr5bm-bhavesh-s-projects.vercel.app/'
     },
     {
         images: "../assets/youtube clone.png",
@@ -20,9 +19,38 @@ const ProjectData = [
         Description:
 
             "I developed a cryptocurrency tracker app where users can monitor the crypto market and visualize market trends through graphs.",
-        link: 'https://shopping-3ub2jr5bm-bhavesh-s-projects.vercel.app/'
+
+        link: "https://bitcoin-project-pi.vercel.app"
     },
 
 ];
 
 export default ProjectData;
+// const ProjectData = [
+//     {
+//         images: "../assets/Ecommerceweb.png",
+//         Name: " Shopping-Website",
+//         Description: "This is a shopping app where I utilized React-Redux-Toolkit and React Router DOM. I implemented features such as add to cart and checkout functionality.",
+
+//         link: "https://bitcoin-project-pi.vercel.app"
+//     },
+//     {
+//         images: "../assets/youtube clone.png",
+//         Name: "YotubeClone",
+//         Description:
+//             "I created a YouTube clone using Rapid API. In this clone, I integrated search functionality.",
+//         link: "https://ytclone-iota.vercel.app/"
+//     },
+//     {
+//         images: "../assets/CryptoCurrency.png",
+
+//         Name: "CryptoCurrency-Tracker",
+//         Description:
+
+//             "I developed a cryptocurrency tracker app where users can monitor the crypto market and visualize market trends through graphs.",
+//         link: 'https://shopping-3ub2jr5bm-bhavesh-s-projects.vercel.app/'
+//     },
+
+// ];
+
+// export default ProjectData;
