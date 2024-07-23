9816117dcedfadd955fd6701a204b5f510d4b9f5
bhavesh saxena <149712436+icobhavesh@users.noreply.github.com>
2024-07-23 19:08:02 +0530

    Update Particles.jsx

--- src/Particles.jsx
+++ src/Particles.jsx
@@ -1,128 +1,17 @@
-// import { useEffect, useMemo, useState } from "react";
-// import Particles, { initParticlesEngine } from "@tsparticles/react";
-// import { loadSlim } from "@tsparticles/slim"
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"
 
-// import "./index.css"
-// const ParticlesBg = () => {
-//     const [init, setInit] = useState(false);
-
-//     // this should be run only once per application lifetime
-//     useEffect(() => {
-//         initParticlesEngine(async (engine) => {
-
-//             await loadSlim(engine);
-
-//         }).then(() => {
-//             setInit(true);
-//         });
-//     }, []);
-
-//     const particlesLoaded = (container) => {
-//         console.log(container);
-//     };
-
-//     const options = useMemo(
-//         () => ({
-//             background: {
-//                 color: {
-//                     value: "#0d47a1",
-//                 },
-//             },
-//             fpsLimit: 120,
-//             interactivity: {
-//                 events: {
-//                     onClick: {
-//                         enable: true,
-//                         mode: "push",
-//                     },
-//                     onHover: {
-//                         enable: true,
-//                         mode: "repulse",
-//                     },
-//                 },
-//                 modes: {
-//                     push: {
-//                         quantity: 4,
-//                     },
-//                     repulse: {
-//                         distance: 200,
-//                         duration: 0.4,
-//                     },
-//                 },
-//             },
-//             particles: {
-//                 color: {
-//                     value: "#ffffff",
-//                 },
-//                 links: {
-//                     color: "#ffffff",
-//                     distance: 150,
-//                     enable: true,
-//                     opacity: 0.5,
-//                     width: 1,
-//                 },
-//                 move: {
-//                     direction: "none",
-//                     enable: true,
-//                     outModes: {
-//                         default: "bounce",
-//                     },
-//                     random: false,
-//                     speed: 6,
-//                     straight: false,
-//                 },
-//                 number: {
-//                     density: {
-//                         enable: true,
-//                     },
-//                     value: 80,
-//                 },
-//                 opacity: {
-//                     value: 0.5,
-//                 },
-//                 shape: {
-//                     type: "circle",
-//                 },
-//                 size: {
-//                     value: { min: 1, max: 5 },
-//                 },
-//             },
-//             detectRetina: true,
-//         }),
-//         [],
-//     );
-
-//     if (init) {
-//         return (
-//             <Particles
-//                 id="tsparticles"
-//                 particlesLoaded={particlesLoaded}
-//                 options={options}
-//             />
-
-//         );
-
-//     }
-
-
-// }
-// export default ParticlesBg 
-import React, { useEffect, useMemo, useState, lazy, Suspense } from "react";
-import { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
-
-import "../index.css";
-
-// Lazy load the Particles component
-const Particles = lazy(() => import("@tsparticles/react"));
-
+import "./index.css"
 const ParticlesBg = () => {
     const [init, setInit] = useState(false);
 
     // this should be run only once per application lifetime
     useEffect(() => {
         initParticlesEngine(async (engine) => {
+
             await loadSlim(engine);
+
         }).then(() => {
             setInit(true);
         });
@@ -136,7 +25,7 @@ const ParticlesBg = () => {
         () => ({
             background: {
                 color: {
-                    value: "#020617",
+                    value: "#0d47a1",
                 },
             },
             fpsLimit: 120,
@@ -156,8 +45,8 @@ const ParticlesBg = () => {
                         quantity: 4,
                     },
                     repulse: {
-                        distance: 300,
-                        duration: 0.2,
+                        distance: 200,
+                        duration: 0.4,
                     },
                 },
             },
@@ -167,7 +56,7 @@ const ParticlesBg = () => {
                 },
                 links: {
                     color: "#ffffff",
-                    distance: 60,
+                    distance: 150,
                     enable: true,
                     opacity: 0.5,
                     width: 1,
@@ -179,7 +68,7 @@ const ParticlesBg = () => {
                         default: "bounce",
                     },
                     random: false,
-                    speed: 2,
+                    speed: 6,
                     straight: false,
                 },
                 number: {
@@ -205,18 +94,129 @@ const ParticlesBg = () => {
 
     if (init) {
         return (
-            <Suspense fallback={<div>Loading...</div>}>
-                <Particles
-                    id="tsparticles"
-                    particlesLoaded={particlesLoaded}
-                    options={options}
-                />
-            </Suspense>
+            <Particles
+                id="tsparticles"
+                particlesLoaded={particlesLoaded}
+                options={options}
+            />
+
         );
+
     }
 
-    return null;
-};
 
-export default ParticlesBg;
+}
+export default ParticlesBg 
+// import React, { useEffect, useMemo, useState, lazy, Suspense } from "react";
+// import { initParticlesEngine } from "@tsparticles/react";
+// import { loadSlim } from "@tsparticles/slim";
+
+// import "../index.css";
+
+// // Lazy load the Particles component
+// const Particles = lazy(() => import("@tsparticles/react"));
+
+// const ParticlesBg = () => {
+//     const [init, setInit] = useState(false);
+
+//     // this should be run only once per application lifetime
+//     useEffect(() => {
+//         initParticlesEngine(async (engine) => {
+//             await loadSlim(engine);
+//         }).then(() => {
+//             setInit(true);
+//         });
+//     }, []);
+
+//     const particlesLoaded = (container) => {
+//         console.log(container);
+//     };
+
+//     const options = useMemo(
+//         () => ({
+//             background: {
+//                 color: {
+//                     value: "#020617",
+//                 },
+//             },
+//             fpsLimit: 120,
+//             interactivity: {
+//                 events: {
+//                     onClick: {
+//                         enable: true,
+//                         mode: "push",
+//                     },
+//                     onHover: {
+//                         enable: true,
+//                         mode: "repulse",
+//                     },
+//                 },
+//                 modes: {
+//                     push: {
+//                         quantity: 4,
+//                     },
+//                     repulse: {
+//                         distance: 300,
+//                         duration: 0.2,
+//                     },
+//                 },
+//             },
+//             particles: {
+//                 color: {
+//                     value: "#ffffff",
+//                 },
+//                 links: {
+//                     color: "#ffffff",
+//                     distance: 60,
+//                     enable: true,
+//                     opacity: 0.5,
+//                     width: 1,
+//                 },
+//                 move: {
+//                     direction: "none",
+//                     enable: true,
+//                     outModes: {
+//                         default: "bounce",
+//                     },
+//                     random: false,
+//                     speed: 2,
+//                     straight: false,
+//                 },
+//                 number: {
+//                     density: {
+//                         enable: true,
+//                     },
+//                     value: 80,
+//                 },
+//                 opacity: {
+//                     value: 0.5,
+//                 },
+//                 shape: {
+//                     type: "circle",
+//                 },
+//                 size: {
+//                     value: { min: 1, max: 5 },
+//                 },
+//             },
+//             detectRetina: true,
+//         }),
+//         [],
+//     );
+
+//     if (init) {
+//         return (
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 <Particles
+//                     id="tsparticles"
+//                     particlesLoaded={particlesLoaded}
+//                     options={options}
+//                 />
+//             </Suspense>
+//         );
+//     }
+
+//     return null;
+// };
+
+// export default ParticlesBg;
 
